e8f6f314b2b77d508214a0c50b28dae199a96880
Nebojsha-2026 <work.petreski@gmail.com>
2026-02-04 00:09:52 +1100

    Update manager.js

--- js/pages/manager.js
+++ js/pages/manager.js
@@ -1,1 +1,6 @@
+import { protect } from "../core/router.js";
+import { logout } from "../core/auth.js";
 
+const profile = await protect("manager");
+
+document.querySelector("#logoutBtn").onclick = logout;
