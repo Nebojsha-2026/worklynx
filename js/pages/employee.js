ceb78640b073f9f132f2c8e16744bf94cb5ccd0a
Nebojsha-2026 <work.petreski@gmail.com>
2026-02-04 00:10:19 +1100

    Update employee.js

--- js/pages/employee.js
+++ js/pages/employee.js
@@ -1,1 +1,6 @@
+import { protect } from "../core/router.js";
+import { logout } from "../core/auth.js";
 
+const profile = await protect("employee");
+
+document.querySelector("#logoutBtn").onclick = logout;
