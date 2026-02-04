7b46138b58bb9d265341d85bd59f22c94c078710
Nebojsha-2026 <work.petreski@gmail.com>
2026-02-04 16:20:28 +1100

    Update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -1,1 +1,11 @@
+// js/core/config.js
+export const CONFIG = {
+  SUPABASE_URL: "https://ljnpugeuyosecggnbapa.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqbnB1Z2V1eW9zZWNnZ25iYXBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxNjgzNTYsImV4cCI6MjA4NTc0NDM1Nn0.Cv4XnbR3O21H4lfqRWyx6ph2qjyqlU7DHGFWLrrNTLo",
 
+  APP_NAME: "WorkLynx",
+
+  // Default UI options
+  DEFAULT_CURRENCY: "AUD",
+  DEFAULT_LANG: "en",
+};
