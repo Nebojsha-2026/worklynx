5e02c14470e4874280d48329169136ca135088f0
Nebojsha-2026 <work.petreski@gmail.com>
2026-02-04 16:04:14 +1100

    Delete js/pages/employee.js

--- js/pages/employee.js
+++
@@ -1,6 +0,0 @@
-import { protect } from "../core/router.js";
-import { logout } from "../core/auth.js";
-
-const profile = await protect("employee");
-
-document.querySelector("#logoutBtn").onclick = logout;
